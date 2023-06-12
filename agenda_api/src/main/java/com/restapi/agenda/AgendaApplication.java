5584651d4511639de25ac02f1263926a723fc111
Guilherme Barros <106363792+GuilhermeBarroz@users.noreply.github.com>
2023-06-12 20:59:35 -0300

    updates

--- agenda_api/src/main/java/com/restapi/agenda/AgendaApplication.java
+++ agenda_api/src/main/java/com/restapi/agenda/AgendaApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgendaApplication {
-
+	public String PORT = System.getenv("port");
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApplication.class, args);
 	}
+
+
 
 }
